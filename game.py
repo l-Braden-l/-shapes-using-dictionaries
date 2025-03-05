f81ce24594b5907cc72c6eca6667622a59d7215a
braden <leachbr787@northwested.org>
2025-03-05 13:50:43 -0500

    Update game.py

--- game.py
+++ game.py
@@ -20,17 +20,6 @@ def handle_events ():
           if event.key == pygame.K_ESCAPE:
              return False
     return True
-
-#-------------------Draw Shapes--------------------------
-def draw_circle(screen, shape):
-   pygame.draw.circle(screen, shape["color"], shape["position"], shape["radius"])
-
-def draw_rect(screen, shape):
-   pygame.draw.rect(screen, shape["color"], (shape["position"][0], shape["position"][1], shape["width"], shape["height"]))
-
-def draw_line(screen, shape):
-   pygame.draw.line(screen, shape["color"], shape["start_pos"], shape["end_pos"], shape["width"])
-#--------------------------------------------------------
    
 def main():
    screen = init_game()
